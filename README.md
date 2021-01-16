059ca6966474642558e869213a7326d029973351
Łukasz Kliś <lukasz@klis.pl>
2021-01-16 12:42:46 +0100

    Init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# RuneLite Plugin Shields
+
+A simple proxy fetching current active installs from plugin hub and returning Shields.io's schema response.
---
+++ app.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+const fastify = require('fastify');
+
+const RUNELITE_VERSION = '1.6.36.2';
+const API_URL = `https://api.runelite.net/runelite-${RUNELITE_VERSION}`;
+const UNKNOWN_PLUGIN = 'unknown';
+
+const api = axios.create({
+  baseURL: API_URL,
+  responseType: 'json',
+});
+
+const app = fastify.fastify({
+  logger: true,
+});
+
+app.get('/active-installs/:plugin', async (request, reply) => {
+  const { plugin } = request.params;
+  let message = UNKNOWN_PLUGIN;
+
+  const plugins = await api.get(`${API_URL}/pluginhub`);
+
+  if (plugins.data && plugins.data[plugin]) {
+    message = plugins.data[plugin].toString();
+  }
+
+  reply
+    .type('application/json')
+    .send({
+      color: 'orange',
+      label: 'active installs',
+      schemaVersion: 1,
+      message,
+    })
+    .code(message !== UNKNOWN_PLUGIN ? 200 : 404);
+});
+
+app.listen(3000, (error, address) => {
+  if (error) {
+    throw error;
+  }
+
+  app.log.info(`server listening on ${address}`);
+});
